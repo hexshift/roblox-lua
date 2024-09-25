28fcad921d2e34a073292f8e1ed4301b59138504
hexshift <epicethan09@icloud.com>
2024-09-26 00:13:17 +0200

    upload

---
+++ random/token.lua
@@ -0,0 +1,10 @@
+--[[
+    @author - larry (@hexshift)
+    @func   - infinite token
+    @game   - https://www.roblox.com/games/115270811776632/i-like-plinko-RNG
+]]
+
+game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game.Players.LocalPlayer.UserId, 1939805878, true)
+
+-- silly unsecure transactions!!! 
+-- i think if you go over something like 1m you won't show on the leaderboard
