a76d1a848f8d43e7452da6d12ff9499cacedbcf0
hexshift <epicethan09@icloud.com>
2024-09-25 23:24:59 +0200

    basic moneyfarm

---
+++ better-music/money-farm.lua
@@ -0,0 +1,11 @@
+--[[
+    @author - larry (@hexshift)
+    @func   - become the real moneyman
+    @game   - https://www.roblox.com/games/18999776880/Better-Music
+]]
+
+while task.wait() do
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimDailyQuest"):FireServer()
+end
+
+-- could this be any more easier LMFAOO
