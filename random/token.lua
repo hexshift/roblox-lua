19eb90a28fdbc7da9363e0ea2d1b9c9dd01805ad
hexshift <epicethan09@icloud.com>
2024-09-26 00:24:30 +0200

    haha funny loop

--- random/token.lua
+++ random/token.lua
@@ -4,7 +4,9 @@
     @game   - https://www.roblox.com/games/115270811776632/i-like-plinko-RNG
 ]]--
 
-game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game.Players.LocalPlayer.UserId, 1939805878, true)
+while task.wait() do
+    game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game.Players.LocalPlayer.UserId, 1939805878, true)
+end
 
 -- silly unsecure transactions!!! 
 -- i think if you go over something like 1m you won't show on the leaderboard
